895958b63cebe4ca94c5f2a9f610f69fa0ff7569
Bogdan C <tunetu_bog@yahoo.com>
2017-05-30 03:56:36 +0300

    Added web crawler

---
+++ src/Command/WebCrawlerCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fetcher\Command;
+
+use Fetcher\WebCrawler;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class WebCrawlerCommand
+ * @package Fetcher\Command
+ */
+class WebCrawlerCommand extends Command
+{
+	protected function configure()
+	{
+		$this
+			->setName('webcrawler')
+			->setDescription('Fetches content')
+			->setHelp(<<<EOT
+Fetches content
+<info>php fetcher fetch</info>
+EOT
+			)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$fetcher = new WebCrawler();
+
+		$fetcher->execute();
+	}
+}
